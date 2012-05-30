6b951c551a6e011599fceec4edf141bff79abf2d
famin_c <cedric.famin@gmail.com>
2012-05-30 17:55:22 +0200

    floatable window added
    Signed-off-by: famin_c <cedric.famin@gmail.com>

--- client_mesh/client_mesh/ViewModels/DashboardModel.cs
+++ client_mesh/client_mesh/ViewModels/DashboardModel.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Data;
 using System.ComponentModel;
+using System.Windows.Input;
+using client_mesh.Utils;
+using System.Windows.Controls;
 
 namespace client_mesh.ViewModels
 {
 	public class DashboardModel : BindableObject
 	{
+        public ICommand OpenProfileWindow { get; private set; }
 		public DashboardModel()
 		{
-			
+			OpenProfileWindow = new RelayCommand((param) => this.OpenProfileWindowBody());
 		}
+
+        private void OpenProfileWindowBody()
+        {  
+        }
 
 	}
 }
